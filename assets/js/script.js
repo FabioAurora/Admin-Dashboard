159bd69e1388c6282de3c65ab484555789cb06a0
Fabio Aurora <fabioauroradev@gmail.com>
2022-12-12 11:57:30 +0000

    changed the class name of profile image

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+const searchCTN = document.querySelector('.form_search-box');
+const searchInput = document.querySelector('#search');
+const searchIcon = document.querySelector('#search-icon');
+
+searchInput.addEventListener('click', function() {
+    searchCTN.classList.add('form_container-focus');
+    searchIcon.setAttribute('style', 'display: inline-flex');
+})
+
+document.addEventListener('click', function(event) {
+    const isClickInside = searchCTN.contains(event.target);
+    if (isClickInside) {
+        return
+    } else {
+        searchCTN.classList.remove('form_container-focus');
+        searchIcon.removeAttribute('style', 'display: inline-flex');
+    };
+  });
